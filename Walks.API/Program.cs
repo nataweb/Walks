531792cf0ff38b24d1399cbe3a410b992853d9a4
nataweb <webmasternatalia777@gmail.com>
2022-10-18 15:40:37 +0300

    End of Section 2

--- Walks.API/Program.cs
+++ Walks.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Walks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WalksDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Walks"));
+
+});
 
 var app = builder.Build();
 
